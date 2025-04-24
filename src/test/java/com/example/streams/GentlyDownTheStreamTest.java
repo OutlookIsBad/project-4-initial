84d3ad1d777339909a62280cf3d83ff92d254d18
Alex Morrison <ale2398183@maricopa.edu>
2025-04-24 15:29:21 -0700

    a

--- src/test/java/com/example/streams/GentlyDownTheStreamTest.java
+++ src/test/java/com/example/streams/GentlyDownTheStreamTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Locale;
 import java.util.stream.Collectors;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 class GentlyDownTheStreamTest {
 
